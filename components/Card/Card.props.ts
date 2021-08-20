1e9780ed1b8aea8685dbe44edb56881c1853b2af
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2021-08-20 12:11:10 +0300

    ADD: create Card component

---
+++ components/Card/Card.props.ts
@@ -0,0 +1,4 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
+export interface CardProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  children: ReactNode;
+}
