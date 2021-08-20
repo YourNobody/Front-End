04214b39d14b4e68af207cc51c9587cec401d3a2
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2021-08-20 19:52:50 +0300

    CHANGE: add to types of Image building

--- components/Image/Image.props.ts
+++ components/Image/Image.props.ts
@@ -1,6 +1,8 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 export interface ImageProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-  src: string;
+  src?: string;
   alt?: string;
   fit?: 'contain' | 'cover' | 'fill' | 'none';
+  isEmpty?: boolean;
+  userInitials: string;
 }
