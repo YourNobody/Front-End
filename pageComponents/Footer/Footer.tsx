8bf1567beec85dfcd34b4538d307cd6664e897a6
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2021-08-24 12:17:37 +0300

    CHANGE: add footer styles

--- pageComponents/Footer/Footer.tsx
+++ pageComponents/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
 
-export const Footer = ({ className, children }: FooterProps): JSX.Element => {
+export const Footer = ({ className }: FooterProps): JSX.Element => {
   return (
     <div className={cn(styles.footer, className)}>
       <div className={styles.appName}>Quiz App</div>
-      <a href="#">Пользовательское соглашение</a>
+      <a href="#">Terms of use</a>
     </div>
   );
 };
